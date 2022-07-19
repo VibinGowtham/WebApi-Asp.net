010875bd4c25bb641e90aa161e77668650199d8f
VibinGowtham <vibingowtham.cs18@bitsathy.ac.in>
2022-07-19 21:32:47 +0530

    Add project files.

---
+++ WebApplication4/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers {
+
+    [ApiController]
+    [Route("getDetails")]
+    public class UserController : ControllerBase
+    {
+       
+        [Route("")]
+        public List<Student> getAll()
+        {
+            Student obj = new Student(1, "Vibin", "Cse", 2022, 22);
+            Student obj1 = new Student(2, "Gowtham", "Mech", 2021, 23);
+            Student obj2 = new Student(3, "Akash", "Cse", 2020, 24);
+
+            List<Student> students = new List<Student>();
+            students.Add(obj1);
+            students.Add(obj2);
+            students.Add(obj);
+            return students;
+        }
+
+        [Route("{id}")]
+        public Student? getDetails(int id)
+        {
+          
+
+            List<Student> students = getAll();
+       
+            
+            foreach(Student student in students)
+            {
+                if (student.Id == id) return student;
+
+            }
+
+            return null;
+        }
+
+
+    }
+}
---
+++ WebApplication4/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿namespace WebApplication4.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public string? Branch { get; set; }
+
+        public int Batch { get; set; }
+
+        public int Age { get; set; }
+
+        public Student(int id, string? name, string? branch, int batch, int age)
+        {
+            Id = id;
+            Name = name;
+            Branch = branch;
+            Batch = batch;
+            Age = age;
+        }
+    }
+}
